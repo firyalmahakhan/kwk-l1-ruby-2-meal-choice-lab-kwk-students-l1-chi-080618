1fbca0b7654273fa232c86495cea5a4e8e9dbe01
firyalmahakhan <firyalmahak@gmail.com>
2018-08-07 16:19:51 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -19,9 +19,7 @@ end
 
 puts snacks("Grapes")
 
-# Call the methods with puts and your own arguments here. Like this:
-puts snacks("Swedish fish")
-## This returns "Any time, is the right time for Sweedish fish!"
+
 
 # call your methods here
 
