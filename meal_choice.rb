5bf2b4c59fca72b41ba8dcc76923f06c2e20dd74
firyalmahakhan <firyalmahak@gmail.com>
2018-08-07 16:19:11 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -17,7 +17,7 @@ def dinner(food="Steak")
   "Any time, is the right time for #{food}!"
 end
 
-
+puts snacks("Grapes")
 
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
